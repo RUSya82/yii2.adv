cc155d3e1827fb3391b2e1527761dd527f1d5867
RUSya82 <mail-spam2012@yandex.ru>
2019-11-21 00:50:47 +0500

    create controllers

---
+++ frontend/controllers/HelloController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class HelloController extends Controller
+{
+
+
+    public function actionIndex()
+    {
+        return $this->render('index',['data' => 'Hello, World!!!']);
+    }
+
+
+}

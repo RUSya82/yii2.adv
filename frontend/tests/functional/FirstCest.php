b9dfba31d0ec77ab11e57df047450805866eb937
RUSya82 <mail-spam2012@yandex.ru>
2019-11-27 19:01:55 +0500

    Create functional tests

--- frontend/tests/functional/FirstCest.php
+++ frontend/tests/functional/FirstCest.php
@@ -6,9 +6,25 @@ class FirstCest
     public function _before(FunctionalTester $I)
     {
     }
-
+    /**
+     * @dataProvider pageProvider
+     */
     // tests
-    public function tryToTest(FunctionalTester $I)
+    public function tryToTest(FunctionalTester $I, \Codeception\Example $data)
     {
+        $I->amOnPage($data['url']);
+        $I->see($data['a'], 'a');
+        $I->click($data['a']);
+        $I->see($data['a'], 'h1');
+    }
+
+    protected function pageProvider() // alternatively, if you want the function to be public, be sure to prefix it with `_`
+    {
+        return [
+            ['url'=>"site/about", 'a'=>"About"],
+            ['url'=>"site/contact", 'a'=>"Contact"],
+            ['url'=>"site/signup", 'a'=>"Signup"],
+            ['url'=>"site/login", 'a'=>"Login"]
+        ];
     }
 }
